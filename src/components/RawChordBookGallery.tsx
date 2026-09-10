d9a837ff34b1c13ff71e30deff49229e2a2f1535
gazaldarbar <gazaldarbar800@gmail.com>
2026-09-10 17:31:23 +0530

    Update RawChordBookGallery.tsx

--- src/components/RawChordBookGallery.tsx
+++ src/components/RawChordBookGallery.tsx
@@ -1,3 +1,109 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import {
+  Bone,
+  BoxGeometry,
+  Color,
+  Float32BufferAttribute,
+  MathUtils,
+  MeshStandardMaterial,
+  Skeleton,
+  SkinnedMesh,
+  SRGBColorSpace,
+  Uint16BufferAttribute,
+  Vector3,
+} from "three";
+import { degToRad } from "three/src/math/MathUtils.js";
+
+const PAGE_WIDTH = 1.28;
+const PAGE_HEIGHT = 1.71;
+const PAGE_DEPTH = 0.003;
+const PAGE_SEGMENTS = 30;
+const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS;
+
+const pageGeometry = new BoxGeometry(
+  PAGE_WIDTH,
+  PAGE_HEIGHT,
+  PAGE_DEPTH,
+  PAGE_SEGMENTS,
+  2
+);
+
+pageGeometry.translate(PAGE_WIDTH / 2, 0, 0);
+
+const position = pageGeometry.attributes.position;
+const vertex = new Vector3();
+const skinIndexes: number[] = [];
+const skinWeights: number[] = [];
+
+for (let i = 0; i < position.count; i++) {
+  vertex.fromBufferAttribute(position, i);
+
+  const x = vertex.x;
+
+  const skinIndex = Math.max(
+    0,
+    Math.floor(x / SEGMENT_WIDTH)
+  );
+
+  const skinWeight =
+    (x % SEGMENT_WIDTH) / SEGMENT_WIDTH;
+
+  skinIndexes.push(
+    skinIndex,
+    skinIndex + 1,
+    0,
+    0
+  );
+
+  skinWeights.push(
+    1 - skinWeight,
+    skinWeight,
+    0,
+    0
+  );
+}
+
+pageGeometry.setAttribute(
+  "skinIndex",
+  new Uint16BufferAttribute(skinIndexes, 4)
+);
+
+pageGeometry.setAttribute(
+  "skinWeight",
+  new Float32BufferAttribute(skinWeights, 4)
+);
+
+const whiteColor = new Color("white");
+
+const easingFactor = 0.5;
+const easingFactorFold = 0.3;
+const insideCurveStrength = 0.18;
+const outsideCurveStrength = 0.05;
+const turningCurveStrength = 0.09;
+
+function TestBook() {
+  return (
+    <group>
+      {/* Book engine will be added here in the next step */}
+    </group>
+  );
+}
+
 export default function RawChordBookGallery() {
-  return null;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "500px",
+      }}
+    >
+      <Canvas>
+        <TestBook />
+      </Canvas>
+    </div>
+  );
 }
